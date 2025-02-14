1ec6d8bf5f6344d8aaa05ba22847c1b87019d037
Habeeb Rahman <dev.habeebrahman@gmail.com>
2025-02-14 14:54:33 +0530

    ``` feat: Enhance model handling and error management
    This commit enhances the handling of models and improves error management within the DeepSeek API client.
    
    The following
    ``````
     changes were made:
    -   Modified `ModelData` to replace `created` with `ownedBy` for better model tracking.
    -   Added `InsufficientBalanceException` for specific balance-related errors.
    -   Enhanced error handling in `DeepSeekAPI` to parse error messages and return more specific exceptions
    ``````
     like `InsufficientBalanceException` for 402 errors and included 503 in server exceptions.
    - Added ModelData, ChatChoice, ChatCompletionResponse and Usage toJson methods.
    - Updated `listModels` output in the example to show models.
    ```

--- example/main.dart
+++ example/main.dart
@@ -1,18 +1,17 @@
 import 'package:deepseek_api/src/deepseek_api.dart';
 import 'package:deepseek_api/src/exceptions.dart';
 import 'package:deepseek_api/src/models.dart';
-import 'package:flutter/foundation.dart';
 
 void main() async {
   // Initialize the DeepSeekClient with your API key
-  final client = DeepSeekAPI(apiKey: 'your_api_key_here');
+  final client = DeepSeekAPI(apiKey: 'sk-31999bba50954807bd0897682a148b95');
 
   try {
     // Example: List available models
     final modelsResponse = await client.listModels();
-    if (kDebugMode) {
+
       print('Available Models: ${modelsResponse.data}');
-    }
+
 
     // Example: Create a chat completion
     final chatRequest = ChatCompletionRequest(
@@ -23,28 +22,21 @@ void main() async {
     );
 
     final chatResponse = await client.createChatCompletion(chatRequest);
-    if (kDebugMode) {
       print('Chat Completion Response: ${chatResponse.choices}');
-    }
   } on BadRequestException catch (e) {
-    if (kDebugMode) {
       print('Bad Request: ${e.message}');
-    }
+
   } on UnauthorizedException catch (e) {
-    if (kDebugMode) {
       print('Unauthorized: ${e.message}');
-    }
+
   } on RateLimitException catch (e) {
-    if (kDebugMode) {
       print('Rate Limit Exceeded: ${e.message}');
-    }
+
   } on ServerException catch (e) {
-    if (kDebugMode) {
       print('Server Error: ${e.message}');
-    }
+
   } on ApiException catch (e) {
-    if (kDebugMode) {
       print('API Error: ${e.message}');
-    }
+
   }
 }
